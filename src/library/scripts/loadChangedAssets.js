059758f5425a8b0805c112d509cecbafa6f7dec4
Chen Peleg <chenpeleg@gmail.com>
2024-05-19 15:54:37 +0300

    added a script to update folder

--- src/library/scripts/loadChangedAssets.js
+++ src/library/scripts/loadChangedAssets.js
@@ -1,10 +1,12 @@
-import { writeFile } from 'node:fs/promises';
 import { Readable } from 'node:stream';
+import { writeFile } from 'fs/promises';
+import { existsSync, mkdirSync } from 'node:fs';
 
-const   updateChangedLibrary = async( )=> {
-  const url =`https://github.com/ChenPeleg/applinks-client/archive/refs/tags/v0.18.zip`
-    const response = await fetch(url)
-    const body = Readable.fromWeb(response.body)
-    await writeFile('document.pdf', body)
-}
-updateChangedLibrary().then()
+const updateChangedLibrary = async () => {
+    const url = `https://github.com/ChenPeleg/applinks-client/archive/refs/tags/v0.18.zip`;
+    const response = await fetch(url);
+    const body = Readable.fromWeb(response.body);
+    !existsSync('temp') && mkdirSync('temp');
+    await writeFile('temp/updatedlib.zip', body);
+};
+updateChangedLibrary().then();
